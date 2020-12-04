8da3788cdf357559a841d116a9aee2fc92e90a94
Joshua M. Clulow <jmc@oxide.computer>
2020-12-04 01:34:07 -0800

    first draft of image template for development image

---
+++ image/README.md
@@ -0,0 +1,4 @@
+This directory contains a rough process for creating a Helios VM image for
+development purposes that will run well under KVM/QEMU on a Linux workstation.
+
+This process must be run on an illumos system.
---
+++ image/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o xtrace
+set -o pipefail
+set -o errexit
+
+TOP=$(cd "$(dirname "$0")" && pwd)
+
+cd "$TOP"
+if [[ ! -d image-builder ]]; then
+	git clone git@github.com:jclulow/illumos-image-builder.git \
+	    image-builder
+fi
+
+if [[ ! -d metadata-agent ]]; then
+	git clone git@github.com:illumos/metadata-agent.git \
+	    metadata-agent
+fi
+
+(cd image-builder && cargo build --release)
+(cd metadata-agent && cargo build --release)
+
+for f in \
+    metadata \
+    metadata.xml \
+    userscript.sh \
+    userscript.xml; do
+	ff="$TOP/templates/files/$f"
+	rm -f "$ff"
+	if [[ $f == metadata ]]; then
+		cp "$TOP/metadata-agent/target/release/$f" "$ff"
+	else
+		cp "$TOP/metadata-agent/$f" "$ff"
+	fi
+done
+
+pfexec "$TOP/image-builder/target/release/image-builder" \
+    build \
+    -d rpool/images \
+    -g helios \
+    -n qemu-ttya \
+    -T "$TOP/templates"
+
