fc78fa47639295a3df26cc59a4ac1151a7dd6e26
Joshua M. Clulow <jmc@oxide.computer>
2020-12-04 01:36:10 -0800

    dont unconditionally recreate; set up user environment in guest

--- create.sh
+++ create.sh
@@ -6,10 +6,22 @@ set -o errexit
 
 VM=helios
 POOL=default
-SIZE=20G
-VCPU=2
-MEM=$(( 2 * 1024 * 1024 ))
+SIZE=30G
+VCPU=6
+MEM=$(( 7 * 1024 * 1024 ))
 TOP=$(cd "$(dirname "$0")" && pwd)
+
+#
+# Check to see if the VM or the root disk volume exists already.  We don't want
+# to make it too easy to accidentally destroy your work.
+#
+if virsh desc "$VM" || virsh vol-info "$VM.qcow2"; then
+	set +o xtrace
+	echo
+	echo "VM $VM exists already; run ./destroy.sh if you want to recreate"
+	echo
+	exit 1
+fi
 
 #
 # Clear out and recreate the temporary directory:
@@ -19,15 +31,68 @@ rm -rf "$TOP/tmp"
 mkdir "$TOP/tmp"
 
 #
-# First, destroy the existing VM and volumes:
+# Before we do anything else, decide if the user has an appropriate SSH key
+# file available.
 #
-virsh destroy "$VM" || true
-virsh undefine "$VM" || true
-virsh vol-delete --pool "$POOL" --vol "$VM.qcow2" || true
-virsh vol-delete --pool "$POOL" --vol "$VM-metadata.cpio" || true
+mkdir -p "$TOP/input/cpio"
+if [[ ! -f "$TOP/input/cpio/authorized_keys" ]]; then
+	if [[ ! -f $HOME/.ssh/authorized_keys ]]; then
+		echo "you have no $HOME/.ssh/authorized_keys file"
+		echo
+		echo "populate $TOP/input/cpio/authorized_keys and run again"
+		echo
+		exit 1
+	fi
+	cp "$HOME/.ssh/authorized_keys" "$TOP/input/cpio/authorized_keys"
+fi
 
 #
-# Next, recreate the metadata volume cpio archive from the proto area:
+# Produce the firstboot script that will run in the new guest to set up a basic
+# user account.  We try to use the same details as the current user, which
+# should ease the use of NFS and SSH if you choose to use them.
+#
+XID=$(id -u)
+XNAME=$(id -un)
+XGECOS=$(getent passwd "$XNAME" | cut -d: -f5)
+cat >"$TOP/input/cpio/firstboot.sh" <<EOF
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o xtrace
+/sbin/zfs create 'rpool/home/$XNAME'
+/usr/sbin/useradd -u '$XID' -g staff -c '$XGECOS' -d '/home/$XNAME' \\
+    -P 'Primary Administrator' -s /bin/bash '$XNAME'
+/bin/passwd -N '$XNAME'
+/bin/mkdir '/home/$XNAME/.ssh'
+/bin/cp /root/.ssh/authorized_keys '/home/$XNAME/.ssh/authorized_keys'
+/bin/chown -R '$XNAME:staff' '/home/$XNAME'
+/bin/chmod 0700 '/home/$XNAME'
+/bin/sed -i \\
+    -e '/^PATH=/s#\$#:/opt/ooce/bin:/opt/ooce/sbin#' \\
+    /etc/default/login
+/bin/ntpdig -S 0.pool.ntp.org || true
+(
+    echo
+    echo
+    banner 'oh, hello!'
+    echo
+    echo "You should be able to SSH to your VM:"
+    echo
+    ipadm show-addr -po type,addr | grep '^dhcp:' |
+        sed -e 's/dhcp:/    ssh $XNAME@/' -e 's,/.*,,'
+    echo
+    echo
+) >/dev/msglog
+exit 0
+EOF
+
+#
+# Set the hostname of the guest to the same name as the VM name:
+#
+echo "$VM" > "$TOP/input/cpio/nodename"
+
+#
+# Next, recreate the metadata volume cpio archive:
 #
 cd "$TOP/input/cpio"
 find . -type f | cpio --quiet -o -O "$TOP/tmp/$VM-metadata.cpio"
@@ -42,7 +107,8 @@ cd "$TOP"
 # Then, recreate the Helios disk from the seed image:
 #
 rm -f "$TOP/tmp/$VM.qcow2"
-qemu-img convert -f raw -O qcow2 "$TOP/input/qemu.raw" "$TOP/tmp/$VM.qcow2"
+qemu-img convert -f raw -O qcow2 "$TOP/input/helios-qemu-ttya.raw" \
+    "$TOP/tmp/$VM.qcow2"
 qemu-img resize "$TOP/tmp/$VM.qcow2" "$SIZE"
 virsh vol-create-as --pool "$POOL" --capacity "$SIZE" --format qcow2 \
     --name "$VM.qcow2"
