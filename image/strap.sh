7721bf2def24ffd3847412f3df6e166b11ec003c
Joshua M. Clulow <jmc@oxide.computer>
2021-04-09 17:53:03 -0700

    allow "strap.sh -f" to restart image creation from scratch

--- image/strap.sh
+++ image/strap.sh
@@ -20,16 +20,41 @@ VARIANT=${VARIANT:-base}
 
 TOP=$(cd "$(dirname "$0")" && pwd)
 
+STRAP_ARGS=()
+
+while getopts 'f' c; do
+	case "$c" in
+	f)
+		#
+		# Use -f to request a full reset from the image builder, thus
+		# effectively destroying any existing files and starting from a
+		# freshly installed set of OS files.
+		#
+		STRAP_ARGS+=( '--fullreset' )
+		;;
+	\?)
+		printf 'usage: %s [-f]\n' "$0" >&2
+		exit 2
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
 cd "$TOP"
 
 for n in 01-strap 02-image 03-archive; do
+	ARGS=()
+	if [[ $n == 01-strap ]]; then
+		ARGS=( "${STRAP_ARGS[@]}" )
+	fi
 	banner "$n"
 	pfexec "$TOP/image-builder/target/release/image-builder" \
 	    build \
 	    -d "$DATASET" \
 	    -g helios \
 	    -n "$VARIANT-$n" \
-	    -T "$TOP/templates"
+	    -T "$TOP/templates" \
+	    "${ARGS[@]}"
 done
 
 ls -lh "$MOUNTPOINT/output/helios-dev-$VARIANT.tar.gz"
