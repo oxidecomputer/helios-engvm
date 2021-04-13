edd116fadb22ce5b8deebf4471ff04b09a3b3f48
Joshua M. Clulow <jmc@oxide.computer>
2021-04-13 10:20:53 -0700

    ramdisk: add basic machine-specific ISO generation

--- image/iso.sh
+++ image/iso.sh
@@ -6,6 +6,8 @@ set -o errexit
 
 DATASET=rpool/images
 MOUNTPOINT="$(zfs get -Ho value mountpoint "$DATASET")"
+MACHINE=${MACHINE:-qemu}
+SERIAL=${SERIAL:-ttya}
 VARIANT=${VARIANT:-iso}
 
 TOP=$(cd "$(dirname "$0")" && pwd)
@@ -16,7 +18,7 @@ pfexec "$TOP/image-builder/target/release/image-builder" \
     build \
     -d "$DATASET" \
     -g helios \
-    -n "qemu-ttya-$VARIANT" \
+    -n "$MACHINE-$SERIAL-$VARIANT" \
     -T "$TOP/templates"
 
-ls -lh "$MOUNTPOINT/output/helios-qemu-ttya-$VARIANT.iso"
+ls -lh "$MOUNTPOINT/output/helios-$MACHINE-$SERIAL-$VARIANT.iso"
