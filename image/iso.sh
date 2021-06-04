e7c48156a532be10f0d5bed7efae9c54281195a8
Joshua M. Clulow <jmc@oxide.computer>
2021-06-04 00:13:59 -0700

    create EFI-capable and USB-hybrid ISO images

--- image/iso.sh
+++ image/iso.sh
@@ -14,6 +14,19 @@ TOP=$(cd "$(dirname "$0")" && pwd)
 
 cd "$TOP"
 
+#
+# Recreate the EFI system partition we include within the ISO file:
+#
+pfexec "$TOP/image-builder/target/release/image-builder" \
+    build \
+    -d "$DATASET" \
+    -g helios \
+    -n "eltorito-efi" \
+    -T "$TOP/templates"
+
+#
+# Build the ISO itself:
+#
 pfexec "$TOP/image-builder/target/release/image-builder" \
     build \
     -d "$DATASET" \
@@ -21,4 +34,6 @@ pfexec "$TOP/image-builder/target/release/image-builder" \
     -n "$MACHINE-$SERIAL-$VARIANT" \
     -T "$TOP/templates"
 
-ls -lh "$MOUNTPOINT/output/helios-$MACHINE-$SERIAL-$VARIANT.iso"
+ls -lh \
+    "$MOUNTPOINT/output/helios-eltorito-efi.pcfs" \
+    "$MOUNTPOINT/output/helios-$MACHINE-$SERIAL-$VARIANT.iso"
