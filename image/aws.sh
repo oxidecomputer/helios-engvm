97d5ddfcf312f06f02381056f43654d7e4dfc479
Joshua M. Clulow <jmc@oxide.computer>
2021-06-30 14:56:10 -0700

    add image templates for AWS EC2 images

---
+++ image/aws.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Produce a raw disk image suitable for use with AWS, based on a seed tar file
+# and some set of additional customisations (e.g., adding a metadata agent or
+# additional OS packages).  Will output an uncompressed raw disk image at,
+# e.g.,
+#
+#	/rpool/images/output/helios-aws-base.raw
+#
+# This tool requires "setup.sh" and "strap.sh" to have been run first.
+#
+
+set -o xtrace
+set -o pipefail
+set -o errexit
+
+DATASET=rpool/images
+MOUNTPOINT="$(zfs get -Ho value mountpoint "$DATASET")"
+VARIANT=${VARIANT:-base}
+
+TOP=$(cd "$(dirname "$0")" && pwd)
+
+cd "$TOP"
+
+pfexec "$TOP/image-builder/target/release/image-builder" \
+    build \
+    -d "$DATASET" \
+    -g helios \
+    -n "aws-$VARIANT" \
+    -T "$TOP/templates"
+
+ls -lh "$MOUNTPOINT/output/helios-aws-$VARIANT.raw"
