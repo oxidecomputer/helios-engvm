49cee65a4ca2b1a9c6bedc3b9074f66e2b5cc10e
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-02-27 22:09:17 -0800

    falcon build from ci parts working
    This also fixes the install issues from doing an onu from a local repo.
    Now we put a p5p on the target image so incorporation dependent packages
    still work.

--- image/falcon-bits.sh
+++ image/falcon-bits.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cd /tmp
+pushd /opt
 
 # get the falcon onu p5p archive from oxidecomputer/os-build ci
 echo "fetching falcon onu archive"
 curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01FWW3YV4SAC4EMH7W2GGV970R/kVJorH8BfiGB9VQ7Ds166vCEKi3cccM6YVjwXlFy2YCaD5nB/01FWW3Z2Q5XZNJYC09J6R4WBHH/01FWWESDSSA3SY4NZEHRTAH6FS/repo.p5p
+mv repo.p5p falcon.p5p
+
+popd
+
+pushd templates/files
+cp /opt/falcon.p5p .
 
 # get the p9kp binary from oxidecomputer/p9fs ci
 echo "fetching p9kp"
