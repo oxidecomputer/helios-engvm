44023ba12c8d822f92dbf7a84349222ef5eb6917
Joshua M. Clulow <jmc@oxide.computer>
2022-04-07 23:09:47 -0700

    add "builder" ramdisk image for buildomat lab use

---
+++ image/templates/files/builder-postboot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cat >/dev/msglog <<EOF
+
+##################################################
+####     #############                          ##
+###  ###  ############                          ##
+##  ### #  ##  ###  ##  Oxide Computer Company  ##
+##  ## ##  ###  #  ###                          ##
+##  # ###  ####   ####    This Station Under    ##
+###  ###  ####  #  ###     Computer Control     ##
+####     ####  ###  ##                          ##
+##################################################
+
+EOF
+
+#
+# Look for scripts in bootfs that we might be expected to run.
+#
+if [[ -f /system/boot/postboot.sh ]]; then
+	exec /bin/bash /system/boot/postboot.sh
+else
+	printf 'WARNING: no /system/boot/postboot.sh\n' >/dev/msglog
+fi
