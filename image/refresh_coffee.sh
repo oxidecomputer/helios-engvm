71483081d99d85ffa13093d74439bac33286a93d
Joshua M. Clulow <jmc@oxide.computer>
2022-07-07 12:29:53 -0700

    images: refresh_coffee.sh needs to discover mountpoint

--- image/refresh_coffee.sh
+++ image/refresh_coffee.sh
@@ -4,17 +4,24 @@
 # boot.
 #
 
+set -o xtrace
+set -o pipefail
+set -o errexit
+
+TOP=$(cd "$(dirname "$0")" && pwd)
+. "$TOP/lib/common.sh"
+
 HTDOCS=/data/www/htdocs/coffee
 
 mkdir -p "$HTDOCS/ramdisk"
 
 cd "$HTDOCS/ramdisk" &&
     tar xvfz \
-    /rpool/images/output/helios-coffee-ramdisk-boot.tar.gz \
+    "$MOUNTPOINT/output/helios-coffee-ramdisk-boot.tar.gz" \
     platform/i86pc/kernel/amd64/unix
 
 mkdir -p "$HTDOCS/ramdisk/platform/i86pc/amd64"
-cp /rpool/images/output/helios-builder-coffee-ttya-ufs.ufs \
+cp "$MOUNTPOINT/output/helios-builder-coffee-ttya-ufs.ufs" \
     "$HTDOCS/ramdisk/platform/i86pc/amd64/boot_archive"
 
 digest -a sha1 \
