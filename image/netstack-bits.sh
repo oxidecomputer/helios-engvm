78b5fa4260028deedb40cd0393f5612823957a68
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-08-25 09:35:04 -0700

    netstack: uninstall entire before installing onu
    Should fix
    - https://github.com/oxidecomputer/falcon-image-netstack/pull/2
    
    Also update artifacts

--- image/netstack-bits.sh
+++ image/netstack-bits.sh
@@ -3,16 +3,14 @@
 pushd /opt
 
 echo "fetching xde onu p5p"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01G410KK2053S6ZXH3R5M7W5GC/SEf36Yj1L7oebc8EbbrL8kIqaieAz4u2blrO40NRPsA7bN9w/01G410KWEXSFANK2YSWEGQ7GXX/01G41BVM0WH4CTCR35ZRRB5NN5/repo.p5p
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GB9KXTVDJMBWE3MYWJAB5RRT/7oGJCNKC1xNtlAK4FlG2pCOh6k2qbwR4jmr5nqNeogI9VP1f/01GB9KY59DXBCPYA3D86HZWX1D/01GB9YQNZNACFYJT162YHT4BPD/repo.p5p
 mv repo.p5p xde.p5p
 
 echo "fetching maghemite p5p"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01G0JAFMZ0VGR9PV1E3H4RB3PR/HOj4QkPA7jBtWP4MtbXf4d7E4mVKKX7Mp9B4R60iU4gqqbE8/01G0JAFX0CVQ97DRA0DEW48D62/01G0JB0YN94HH4GKAAQPRERWMY/maghemite-0.1.110.p5p
-mv maghemite-0.1.110.p5p mg.p5p
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GAPNJQCDD94YY1Z5S3CKKGZM/9BCSks8CwdgOcobDZuR2piLumE6zUF1DjzlQMdIPYUUdybi8/01GAPNKADWRVR6BQ2BGN9XPE3V/01GAPP6TFPAW5S2K7DRCRTFMH1/mg.p5p
 
 echo "fetching opte p5p"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01G0DM53XR4E008D6ET5T8DXP6/wBWo0Jsg1AG19toIyAY23xAWhzmuNKmAsF6tL18ypZODNuHK/01G0DM5DMFN9292PRSGF0EBATG/01G0DMGDKHGEJMKPKZPWV06MCZ/opte-0.1.56.p5p
-mv opte-0.1.56.p5p opte.p5p
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GASNZZCMBSSGNJ861HYW3VV7/60lblVYQwvxaLk1cAadzZcNbXSsrlpLgJMimKzntyA09gHq2/01GASP08C5P83J2ZC9HWWZVZSX/01GASPC8VZHMWVBG54X80V0KGP/opte.p5p
 
 popd
 
@@ -23,5 +21,4 @@ cp /opt/opte.p5p .
 
 # get the p9kp binary from oxidecomputer/p9fs ci
 echo "fetching p9kp"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01FWVH16QWB4FCKHNDADG2F8ZN/FmPrmgdSkn44eowjPWFoqikXynga4oJYhHcoBAwjB8E531tv/01FWVH1E7GXY0TRVZYGT7GJ789/01FWVHADFJEW082REBMJMJDC5R/p9kp
-
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GBARQTZCWBK1CDFCD9CZH0QK/bfi1j8ZkKCN2XaJ8FfnQ7W5CQYWyAk61P8bDiyT5Xi10ttNq/01GBARR2K3RR8G4T56A419EWFB/01GBAS16EZZ7B12DZB45TPR6TC/p9kp
