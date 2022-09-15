0faa2267ce23acb09c0ca15a568e4b96cf89b7e6
Joshua M. Clulow <jmc@oxide.computer>
2022-09-15 15:29:14 -0700

    image: gimlet boot should carry image hash in cpio

--- image/experiments/wackadoo/mk.sh
+++ image/experiments/wackadoo/mk.sh
@@ -14,20 +14,22 @@ HELIOS=${HELIOS:-/ws/helios}
 PHBL=${PHBL:-/ws/phbl}
 AMD_HOST_IMAGE_BUILDER=${AMD_HOST_IMAGE_BUILDER:-/ws/amd-host-image-builder}
 PINPRICK=${PINPRICK:-/ws/pinprick/target/release/pinprick}
+BANNER=${BANNER:-/ws/prombanner/target/release/prombanner}
+MKIMAGE=${MKIMAGE:-/ws/bootserver/target/release/mkimage}
 
 OUTDIR=/tmp/wackadoo.$LOGNAME
 
 case "$1" in
 full)
-	banner pkgs
+	"$BANNER" pkgs
 	"$HELIOS"/helios-build onu -P -g "$GATE" -s wackadoo
 
-	banner strap
+	"$BANNER" strap
 	VARIANT=ramdisk ./strap.sh -S -E \
 	    -O "$HELIOS/tmp/onu.wackadoo/repo.redist"
 	;;
 quick)
-	banner quick
+	"$BANNER" quick
 	VARIANT=ramdisk ./strap.sh -S -E -A \
 	    -O "$HELIOS/tmp/onu.wackadoo/repo.redist"
 	;;
@@ -40,7 +42,7 @@ esac
 
 case "$1" in
 full|quick)
-	banner zfs
+	"$BANNER" zfs
 	./zfs.sh -O -E
 	;;
 esac
@@ -50,23 +52,35 @@ mkdir -m 0700 -p "$OUTDIR"
 
 ROOT="$MOUNTPOINT/work/helios/ramdisk-onu"
 
-banner cpio
-(cd "$ROOT" && cpio -qo -H odc) <"$WACKADOO/cpiofiles.txt" >"$OUTDIR/cpio"
+"$BANNER" image
+"$MKIMAGE" \
+    -i "$MOUNTPOINT/output/helios-generic-onu-ttya-zfs.raw" \
+    -o "$OUTDIR/zfs.img"
+
+#
+# The image contains a SHA-256 hash that we need to extract and include in the
+# CPIO archive.
+#
+dd if="$OUTDIR/zfs.img" bs=1 count=32 iseek=24 > "$OUTDIR/boot_image_csum"
+
+"$BANNER" cpio
+(cd "$ROOT" && cpio -qo -H odc -O "$OUTDIR/cpio") <"$WACKADOO/cpiofiles.txt"
+(cd "$OUTDIR" && cpio -qo -H odc -AO "$OUTDIR/cpio") <<<boot_image_csum
 
 #
 # Create compressed versions of unix and the cpio for nanobl-rs:
 #
-banner cpio.z
+"$BANNER" cpio.z
 "$PINPRICK" "$OUTDIR/cpio" >"$OUTDIR/cpio.z"
-banner unix.z
+"$BANNER" unix.z
 "$PINPRICK" "$ROOT/platform/oxide/kernel/amd64/unix" >"$OUTDIR/unix.z"
 
 #
 # Create the reset image for the Gimlet SPI ROM:
 #
-banner phbl
+"$BANNER" phbl
 (cd "$PHBL" && cargo xtask build --release --cpioz "$OUTDIR/cpio.z")
-banner rom
+"$BANNER" rom
 (cd "$AMD_HOST_IMAGE_BUILDER" &&
     target/debug/amd-host-image-builder \
     -B amd-firmware/GN/1.0.0.1 \
@@ -74,8 +88,5 @@ banner rom
     --config etc/milan-gimlet-b.efs.json5 \
     --output-file "$OUTDIR/rom" \
     --reset-image "$PHBL/target/x86_64-oxide-none-elf/release/phbl")
-
-banner zfs
-cp "$MOUNTPOINT/output/helios-generic-onu-ttya-zfs.raw" "$OUTDIR/zfs"
 
 ls -lh "$OUTDIR"
