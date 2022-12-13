f5ae5ee7342014511792320410331e7d411c3517
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-12-12 16:31:11 -0800

    falcon base image from oxide/master, add stlouis

--- image/falcon-bits.sh
+++ image/falcon-bits.sh
@@ -3,8 +3,8 @@
 pushd /opt
 
 # get the falcon onu p5p archive from oxidecomputer/os-build ci
-echo "fetching falcon onu archive"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01FWW3YV4SAC4EMH7W2GGV970R/kVJorH8BfiGB9VQ7Ds166vCEKi3cccM6YVjwXlFy2YCaD5nB/01FWW3Z2Q5XZNJYC09J6R4WBHH/01FWWESDSSA3SY4NZEHRTAH6FS/repo.p5p
+echo "fetching helios onu archive"
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GM3VH03DW8RVV7GBWQK6ZBHP/A08P7kn1WfARQbmnGiJpkiBtpvGDskDRX7JEPccQgUGPZUx8/01GM3VH8X2DSV58EPX3XVM96G9/01GM46CC3XVCCEQTRXG385DNK1/repo.p5p
 mv repo.p5p falcon.p5p
 
 popd
@@ -14,4 +14,4 @@ cp /opt/falcon.p5p .
 
 # get the p9kp binary from oxidecomputer/p9fs ci
 echo "fetching p9kp"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01FWVH16QWB4FCKHNDADG2F8ZN/FmPrmgdSkn44eowjPWFoqikXynga4oJYhHcoBAwjB8E531tv/01FWVH1E7GXY0TRVZYGT7GJ789/01FWVHADFJEW082REBMJMJDC5R/p9kp
+curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GBNATSKDE01G1K389NNCE9TT/LAv0ABhVuxOcDfLTwYVcb58WO6UpvDKISnzJSFb4HdzeN3WA/01GBNAV15R3SDFJGS495NW00DT/01GBNB3RP5YT2AQ9DH1CAHTVG2/p9kp
