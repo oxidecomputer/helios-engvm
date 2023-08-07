5b2c1ffbbfde8e7a6facf6bd99a460f2fec40f98
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-08-07 11:04:04 -0700

    update to helios-2

--- image/falcon-bits.sh
+++ image/falcon-bits.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-pushd /opt
-
-# get the falcon onu p5p archive from oxidecomputer/os-build ci
-echo "fetching helios onu archive"
-curl -OL https://buildomat.eng.oxide.computer/wg/0/artefact/01GM3VH03DW8RVV7GBWQK6ZBHP/A08P7kn1WfARQbmnGiJpkiBtpvGDskDRX7JEPccQgUGPZUx8/01GM3VH8X2DSV58EPX3XVM96G9/01GM46CC3XVCCEQTRXG385DNK1/repo.p5p
-mv repo.p5p falcon.p5p
-
-popd
-
 pushd templates/files
-cp /opt/falcon.p5p .
 
 # get the p9kp binary from oxidecomputer/p9fs ci
 echo "fetching p9kp"
