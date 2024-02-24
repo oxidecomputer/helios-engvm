4b5ade1741c4aa25d944356029bcd2d70a5f0b50
Rain <rain@sunshowers.io>
2024-02-23 17:12:09 -0800

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.6-beta.1

--- aws/gen_userdata.sh
+++ aws/gen_userdata.sh
@@ -18,6 +18,26 @@ if [[ "$(uname)" == Darwin ]]; then
 else
 	XGECOS=$(getent passwd "$XNAME" | cut -d: -f5)
 fi
+
+# Below, the uuencode is changed via awk so that the permissions on the file
+# are always 600. Over on the Helios side, we're going to write
+# .ssh/authorized_keys so that its group is `staff`. Since the user's primary
+# group ID isn't the same as the user ID, for sshd to accept
+# `.ssh/authorized_keys`, it must not be group-writable.
+#
+# But some setups may be different -- in particular, on Ubuntu, by default, the
+# group ID is the same as the user ID, and the default umask is 002 (on most
+# Unixes the default umask is 022). This means that the `.ssh/authorized_keys`
+# file may be group-writable. (sshd is fine with that arrangement too, since
+# it's still not writable by others).
+#
+# When run on Ubuntu, uuencode isn't aware of this subtlety, though (nor would
+# it be reasonable to expect uuencode to be). So it produces a file with
+# group-writable permissions. The user will then fail to log in via ssh, and be
+# left confused.
+#
+# The awk command fixes that by always forcing permissions to 600.
+
 cat <<EOF
 #!/bin/bash
 set -o errexit
@@ -30,7 +50,7 @@ echo 'Just a moment...' >/dev/msglog
 /bin/passwd -N '$XNAME'
 /bin/mkdir '/home/$XNAME/.ssh'
 /bin/uudecode <<'EOSSH'
-$(uuencode -m "$HOME/.ssh/authorized_keys" "/home/$XNAME/.ssh/authorized_keys")
+$(uuencode -m "$HOME/.ssh/authorized_keys" "/home/$XNAME/.ssh/authorized_keys" | awk 'NR == 1 { $2 = "600" } { print }')
 EOSSH
 /bin/chown -R '$XNAME:staff' '/home/$XNAME'
 /bin/chmod 0700 '/home/$XNAME'
